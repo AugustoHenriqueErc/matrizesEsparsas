726a78f8ffa9c1d7e94626df26abe2a9c4536005
Augusto Henrique <aaugustohenrique@05gmail.com>
2025-07-16 10:35:35 -0300

    mudanças nas classes MySparseMatrix, adicionando métodos para percorrer lateralmente e verticalmente, adicionando no final da linha e coluna.

--- src/sparseMatrix/MySparseMatrix.java
+++ src/sparseMatrix/MySparseMatrix.java
@@ -19,11 +19,13 @@ public class MySparseMatrix implements SparseMatrix {
         aux.getNextRight().setNextRight(cell);
         cell.setNextRight(head);
     }
-    private void addDown(Cell c){ //tentar implementar metodo de goRight() passando hLength como parametro
+    private void addDown(Cell cell){ //tentar implementar metodo de goRight() passando hLength como parametro
         Cell aux = head;
         do {
-
-        }while (aux.getNextDown()==head)
+            aux.setNextDown(aux.getNextDown());
+        }while (aux.getNextDown()==head);
+        aux.getNextDown().setNextDown(cell);
+        aux.setNextDown(head);
     }
 
     private Cell goRight(int distance) { //throws NullPointerException caso distance seja 0
@@ -52,22 +54,20 @@ public class MySparseMatrix implements SparseMatrix {
     // j = horizontal position;
     public void insert(float v, int i, int j){
         Cell cell = new Cell(v,i,j);
-        if(getVLength() == 0){
-            for(int k = 0; k < j; k++){
+        if(getVLength() == 0 && gethLength() == 0){
+            for(int k = 0; k < j; k++){ //Iterations to create the amout of grid lines
 
                 vLength++;
+            }
+            for(int k = 0; k < i; k++){ //Iterations to create the amount of grid collums
+
+                hLength++;
             }
         }
     }
     @Override
     public void print() {
-        Cell aux = head.getNextDown();
-        for(int i = 0; i < hLength; i++){
-            do{
-                aux.se
-            }while(aux != head)
-        }
-
+        for(int = 0)
     }
     public void multiply(MySparseMatrix A, MySparseMatrix B){
 
